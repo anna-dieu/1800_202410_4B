e524828a053568c51cd82e4d71597bfcb2b4aeb0
anna-dieu <adao10@my.bcit.ca>
2024-03-16 16:31:17 -0700

    added firebase authentication.

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,21 @@
-function sayHello() {
-    
-}
-//sayHello();
+//---------------------------------
+// Your own functions here
+//---------------------------------
 
+
+function sayHello() {
+    //do something
+}
+//sayHello();    //invoke function
+
+//------------------------------------------------
+// Call this function when the "logout" button is clicked
+//-------------------------------------------------
+function logout() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        console.log("logging out user");
+      }).catch((error) => {
+        // An error happened.
+      });
+}
